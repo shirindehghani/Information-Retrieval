7fccf6abb56bd7cf99a3e93bb1529ce8875ce206
Shirin Dehghani <shirin.dehghani1996@gmail.com>
2023-12-22 22:49:48 +0330

    bug fixes

--- Retrieval.py
+++ Retrieval.py
@@ -8,7 +8,30 @@ from log_handler.Setup import logger
 import json
 import os
 
-
+class exact_retrieval():
+    def __init__(self):
+        return None
+    
+    def load_and_split_contents(self):
+        loader=PyPDFLoader(self.path)
+        docs=loader.load()
+        text_splitter=RecursiveCharacterTextSplitter(chunk_size=self.chunk_size_exact,
+                                                     chunk_overlap=self.chunk_overlap,
+                                                     add_start_index=True)
+        result=text_splitter.split_documents(docs)
+        final_result=[result[i].page_content for i in range(len(result))]
+        return final_result
+    
+    def retrieve_doc(self, query):
+        final_result=self.load_and_split_contents()
+        scores=[]
+        for i in final_result:
+            if query in i:
+                scores.append(1)
+            else: scores.append(0)
+        return [final_result[i] for i in range(len(final_result)) if scores[i]==1]
+    
+    
 class openAI():
     def __init__(self):
         self.api_key=None
@@ -35,15 +58,6 @@ class openAI():
             self.embedding_model=my_config['embedding_model']
             self.top_k=my_config['top_k']
 
-    def retrieve_doc(self, query):
-        final_result=self.load_and_split_contents()
-        scores=[]
-        for i in final_result:
-            if query in i:
-                scores.append(1)
-            else: scores.append(0)
-        return [final_result[i] for i in range(len(final_result)) if scores[i]==1]
-
     def load_and_split_contents(self):
         loader=PyPDFLoader(path=self.path)
         docs=loader.load()
@@ -58,16 +72,3 @@ class openAI():
         docs=faiss_index.similarity_search(query, k=self.top_k)
         docs_ret=[docs[i].page_content for i in range(len(docs))]
         return docs_ret
-
-class exact_retrieval():
-    def __init__(self):
-        return None
-    def load_and_split_contents(self):
-        loader=PyPDFLoader(self.path)
-        docs=loader.load()
-        text_splitter=RecursiveCharacterTextSplitter(chunk_size=self.chunk_size_exact,
-                                                     chunk_overlap=self.chunk_overlap,
-                                                     add_start_index=True)
-        result=text_splitter.split_documents(docs)
-        final_result=[result[i].page_content for i in range(len(result))]
-        return final_result
