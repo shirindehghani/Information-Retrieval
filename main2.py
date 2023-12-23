887c869c0195d6faf740789e9e0a2d7ddb3cadf1
Shirin Dehghani <shirin.dehghani1996@gmail.com>
2023-12-23 19:16:40 +0330

    bug fixes

--- main2.py
+++ main2.py
@@ -1,4 +1,4 @@
 from IR_models.Retrieval import query_builder_andRetrieve
 
-response=query_builder_andRetrieve("mom text", api_key="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")
+response=query_builder_andRetrieve("mom text", api_key="")
 print(len(response))
